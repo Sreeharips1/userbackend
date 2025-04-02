222a03e4f01fd99d6b17779dd5e12a7620d3aa1a
Sreehari P Shaiju <133079958+Sreeharips1@users.noreply.github.com>
2025-04-02 16:05:22 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const { createOrder, checkPaymentStatus, getPaymentDetails, getPaymentDetailsbyID } = require('../controllers/paymentController');
+const { createOrder, checkPaymentStatus, getPaymentDetails, getPaymentDetailsbyID, editPaymentDetails, generateBarcode} = require('../controllers/paymentController');
 
 const router = express.Router();
 
-router.post('/create-order', createOrder);
+router.post('/create-order/:membershipID', createOrder);
+
 router.post('/status', checkPaymentStatus);
 router.get('/status', checkPaymentStatus);
 router.get('/payment-details/:membershipID', getPaymentDetailsbyID); // New route for getting payment details
+router.get('/barcode/:membershipID',generateBarcode)
 router.get('/payment-details',getPaymentDetails);
-
+router.put('/edit/:membershipID',editPaymentDetails);
 module.exports = router;
